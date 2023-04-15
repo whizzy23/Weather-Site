554da0f5fa1518a7b8de6918b9da570e5e0e6dec
Pitamber Paudel <pitamberpaudel2003@gmail.com>
2023-04-16 00:56:25 +0530

    first commit

--- script.js
+++ script.js
@@ -8,15 +8,22 @@ function callapi(cname){
         return response.json()
     })
     .then(data => {
+
         console.log(data)
         let tempr="Temp : "+(data.main.temp-273.15).toFixed(2)+"°C"
         let winds = "Wind Speed : " + data.wind.speed
         let feeltemp = "Feels Like : "+(data.main.feels_like-273.15).toFixed(2)+"°C"
+        let mintemp = "Min Temp : "+(data.main.temp_min-273.15).toFixed(2)+"°C"
+        let maxtemp = "Max Temp : "+(data.main.temp_max-273.15).toFixed(2)+"°C"
         let clouds = "Clouds : " + data.clouds.all
+
         document.getElementById("temp").innerHTML = tempr
         document.getElementById("ftemp").innerHTML = feeltemp
+        document.getElementById("mintemp").innerHTML = mintemp
+        document.getElementById("maxtemp").innerHTML = maxtemp
         document.getElementById("wind").innerHTML = winds
         document.getElementById("sky").innerHTML = clouds
+
     })
     .catch(err => {
         console.log(err);
