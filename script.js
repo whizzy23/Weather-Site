2a6dc4e87365bbcbf645f147a9e329b723de791c
Pitamber Paudel <pitamberpaudel2003@gmail.com>
2023-04-21 03:07:39 +0530

    improvements done

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+function inp(){
+    var input = document.getElementById("cityname")
+    input.addEventListener("keydown", (e)=> {
+     if (e.key === "Enter") {  
+       var cname = e.target.value
+       callapi(cname)
+}})}
+inp()
+
+
 function func(){
     let cname = document.getElementById("cityname").value
-    return callapi(cname)
+    callapi(cname)
 }
+
+
 function callapi(cname){
     fetch("https://api.openweathermap.org/data/2.5/weather?q="+cname+"&appid=ffc62ccd1d119915112987d5a4b6fe4a")
     .then(response => {
@@ -24,14 +36,13 @@ function callapi(cname){
         let feeltemp = "Feels Like : "+(data.main.feels_like-273.15).toFixed(2)+"°C"
         let mintemp = "Min Temp : "+(data.main.temp_min-273.15).toFixed(2)+"°C"
         let maxtemp = "Max Temp : "+(data.main.temp_max-273.15).toFixed(2)+"°C"
-        let clouds = "Clouds : " + data.clouds.all
-
+        let sky = data.weather[0].main
         document.getElementById("temp").innerHTML = tempr
         document.getElementById("ftemp").innerHTML = feeltemp
         document.getElementById("mintemp").innerHTML = mintemp
         document.getElementById("maxtemp").innerHTML = maxtemp
         document.getElementById("wind").innerHTML = winds
-        document.getElementById("sky").innerHTML = clouds
+        document.getElementById("sky").innerHTML = sky
 
         selectImage (data)
 
@@ -99,12 +110,42 @@ function selectImage (data){
 
 }
 
-function timebg(){
+function clock(){ 
+    var date=new Date()
+    var D=date.getDay()
+    if(D==0){
+        Day="Sunday"
+    }
+    else if(D==1){
+        Day="Monday"
+    }
+    else if(D==2){
+        Day="Tuesday"
+    }
+    else if(D==3){
+        Day="Wednesday"
+    }
+    else if(D==4){
+        Day="Thursday"
+    }
+    else if(D==5){
+        Day="Friday"
+    }
+    else if(D==6){
+        Day="Saturday"
+    }
+    
+    var d = date.getDate()+"-"+(date.getMonth()+1)+"-"+ date.getFullYear() 
+    var t = date.getHours()+":"+date.getMinutes();
+    document.getElementById("day").innerHTML = Day
+    document.getElementById("date").innerHTML = d
+    document.getElementById("time").innerHTML = t
+}
 
+function timebg(){
     var bgImage=''
     var d = new Date()
     var n = d.getHours()
-    console.log(n)
     if (n>=5 && n<6){
         // If time is between 5AM - 6AM sunrise theme to ‘body’
         bgImage= 'sunrise.jpg'}
@@ -120,6 +161,10 @@ function timebg(){
       bgImage= 'day.jpg'}
     
     document.body.style.backgroundImage = "url(" + bgImage + ")"
+
+    clock()
+    
     }
     
-    timebg()
+timebg()
+
