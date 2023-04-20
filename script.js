600fcf159cd97616df4a08c12914cc05c9784443
Pitamber Paudel <pitamberpaudel2003@gmail.com>
2023-04-21 00:23:55 +0530

    weatherbg done

--- script.js
+++ script.js
@@ -49,18 +49,17 @@ var weatherImage = "";
 
   var imagePhoto = {
 
-    thunder: "https://s-media-cache-ak0.pinimg.com/originals/2e/43/73/2e4373184057ab029b5ca15aca484b09.jpg",
-    rainy: "https://i.ytimg.com/vi/q76bMs-NwRk/maxresdefault.jpg",
-    cloudy: "https://static1.squarespace.com/static/57523357c2ea515ccf6c1adb/58dcea75bebafb06e997da9c/58dcece61e5b6cf38585d46b/1490873606398/mostly+cloudy.jpg",
-    snow: "https://static.bhphotovideo.com/explora/sites/default/files/Correct.jpg",
-    clear: "https://images.unsplash.com/photo-1601297183305-6df142704ea2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80.jpg",
-    drizzle: "https://s-media-cache-ak0.pinimg.com/736x/e4/43/f5/e443f59b4f03dd487d090a279c2f08ab--rain-drops-water-drops.jpg",
-    haze: "https://cdn.pixabay.com/photo/2022/09/13/13/02/hills-7451872_960_720.jpg",
-    tornado: "https://i.ytimg.com/vi/5QnsRXUbsK4/maxresdefault.jpg",
-    cold: "https://static.pexels.com/photos/374/snow-dawn-sunset-winter.jpg",
-    windy: "https://ak9.picdn.net/shutterstock/videos/4337360/thumb/1.jpg",
-    hail: "https://s-media-cache-ak0.pinimg.com/236x/7c/60/3d/7c603d9183ff32c92330780a2ebdcfca--rainy-weather-rainy-days.jpg",
-    sunny: "https://i.ytimg.com/vi/rG9fev-m0ag/maxresdefault.jpg"
+    thunder: "thunder.jpg",
+    rainy: "rain.jpg",
+    cloudy: "cloudy.jpg",
+    snow: "snow.jpg",
+    drizzle: "drizzle.jpg",
+    haze: "haze.jpg",
+    tornado: "tornado.jpg",
+    cold: "cold.jpg",
+    windy: "windy.jpg",
+    hail: "hail.jpg",
+    sunny: "sunny.jpg"
 };
 
 
